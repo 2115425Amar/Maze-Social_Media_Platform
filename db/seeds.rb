236a8f5855cb0396d4f771615eb977a9f3f2eb7d
JarvisAmar <amar.gupta@jarvis.consulting>
2025-05-05 11:21:08 +0530

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
-
-# db/seeds.rb
-admin = User.find_or_create_by(email: 'newadmin2@gmail.com') do |user|
-  user.first_name = 'newadmin'
-  user.last_name = '2'
-  user.phone_number = '8601082965'
-  user.password = 'newadmin#1234'
-  user.password_confirmation = 'newadmin#1234'
-end
-
-admin.add_role(:admin) # Assuming you are using Rolify
-# admin.save!
+admin = User.create!(
+  email: "amar2115425@akgec.ac.in",
+  password: "password",
+  first_name: "Receptionist",
+  last_name: "Admin",
+  phone_number: "9876543555"
+)
+admin.add_role(:admin)
