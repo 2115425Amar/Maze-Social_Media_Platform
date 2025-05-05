43e9e3dc819bbba76a2c73361638b630dd3561b1
JarvisAmar <amar.gupta@jarvis.consulting>
2025-05-05 12:10:53 +0530

    update

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,11 +1,25 @@
-def create
-  @post = Post.find(params[:post_id])
-  @comment = @post.comments.build(comment_params)
-  @comment.user = current_user
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      def index
+        render json: { message: "index action works!" }
+      end
 
-  if @comment.save
-    render json: { message: "Comment added!", comment: @comment }, status: :created
-  else
-    render json: { error: "Comment could not be created", reasons: @comment.errors.full_messages }, status: :unprocessable_entity
+      def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.build(comment_params)
+        @comment.user = current_user
+      
+        if @comment.save
+          render json: { message: "Comment added!", comment: @comment }, status: :created
+        else
+          render json: { error: "Comment could not be created", reasons: @comment.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+      
+      # Add other actions here
+    end
   end
 end
+
+
